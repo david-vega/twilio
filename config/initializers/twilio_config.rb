f8b2451d7bd0fcc792e7bb60a4285fb130ccd82a
David Vega <zetacu@gmail.com>
2013-08-29 10:36:25 -0500

    some extra configurations

--- config/initializers/twilio_config.rb
+++ config/initializers/twilio_config.rb
@@ -1,5 +1,5 @@
 TWILIO_CONFIG = YAML.load_file("#{::Rails.root}/config/twilio.yml")[::Rails.env]
 
-CarterRails::Application.config.account_sid = TWILIO_CONFIG['sid']
-CarterRails::Application.config.auth_token = TWILIO_CONFIG['token']
-CarterRails::Application.config.phone_number = TWILIO_CONFIG['phone']
+TwilioApp::Application.config.account_sid = TWILIO_CONFIG['sid']
+TwilioApp::Application.config.auth_token = TWILIO_CONFIG['token']
+TwilioApp::Application.config.phone_number = "+#{TWILIO_CONFIG['phone']}"
