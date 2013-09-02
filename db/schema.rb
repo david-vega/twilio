b59cc894e057f359550b2eda156ed9971d1f7ac1
David Vega <zetacu@gmail.com>
2013-09-02 13:42:33 -0500

    add phone number to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130902172501) do
+ActiveRecord::Schema.define(:version => 20130902181537) do
 
   create_table "users", :force => true do |t|
     t.string   "nickname"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "phone_number"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
